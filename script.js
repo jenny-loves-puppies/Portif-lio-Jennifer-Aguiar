78e9fc8d435c7d682fb4294a0458739d728033cd
Jennifer_Aguiar <137212752+jenny-loves-puppies@users.noreply.github.com>
2025-01-31 22:26:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Exemplo: Mostrar uma mensagem no console
 console.log("Bem-vindo(a) ao Meu Portfólio!");
+document.getElementById("modo-escuro").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+
+    // Salvar a preferência no navegador
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("modoEscuro", "ativado");
+    } else {
+        localStorage.setItem("modoEscuro", "desativado");
+    }
+});
+
+// Manter o modo escuro ativado ao recarregar a página
+if (localStorage.getItem("modoEscuro") === "ativado") {
+    document.body.classList.add("dark-mode");
+}
